27dcc68349812a0f8ed50c1b4fbca2bf60bd92e1
David Shen <zdshen99@gmail.com>
2020-10-15 13:11:52 -0600

    Trial
    this is the first vesion of the project.
    -set up project
    -work on an example

---
+++ david/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Fetch data with API</h1>
+    <EmployeeList />
+  </div>
+</template>
+<script>
+  import EmployeeList from './components/employee'
+  export default {
+      name: "App",
+      components: {
+        EmployeeList
+      }
+    }
+</script>
+
+<style>
+</style>
