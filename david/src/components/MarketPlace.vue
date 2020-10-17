d5e2ae5c3c4c4f213006d47be5768ae7d1fe0991
David Shen <zdshen99@gmail.com>
2020-10-17 16:32:21 -0600

    modified UI
    changed UI to make sure that the picture would be the same size and also changed API to display correct data

--- david/src/components/MarketPlace.vue
+++ david/src/components/MarketPlace.vue
@@ -1,32 +1,78 @@
 <template>
-  <div>
-    <b-card-group deck>
-      <div v-for="item in list" :key="item.asset_id">
-      <b-card :title="item.name" v-bind:img-src="'https://ipfs.io/ipfs/'+item.data.img" class="picture scaleDownImg">
-        <b-card-text>
-         {{item.asset_id}}<br />
-          {{item.owner}}
-        </b-card-text>
-        <b-button variant="outline-primary">Info</b-button>
-        <b-button variant="outline-primary">button</b-button>
-      </b-card>
+  <div class="main-block w-100">
+    <div class="row">
+        <div class="grid text-center px-0 MobileCardDesign">
+          <div v-for="item in list" :key="item.asset_id">
+            <div class="large-card">
+              <div class="picture-wrapper">
+                <b-card>
+                  <b-card-img v-bind:src="'https://ipfs.io/ipfs/'+item.assets[0].data.img" style="height: 150px">
+                  </b-card-img>
+                </b-card>
+                <b-card-text>
+                  <p style="font-size: 15px">{{item.price.amount}}&nbsp;{{item.price.token_symbol}}</p><br>
+                  <p>{{item.assets[0].name}}</p>
+                </b-card-text>
+              </div>
+            </div>
+
+          </div>
       </div>
-    </b-card-group>
+    </div>
   </div>
 </template>
 <style scoped>
-.picture {
-  width: 130px;
-  height: 156px;
+
+.truncate-with-css-ellipsis {
+  white-space: nowrap;
   overflow: hidden;
-  display: table-cell;
-  vertical-align: middle;
+  text-overflow: ellipsis;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(20px, auto);
+  grid-gap: 1rem;
+}
+.main-block {
+  padding: 35px 49px 33.5px 51px;
+}
+
+.large-card {
+  width: 180px;
+  border-radius: 20px;
+  box-shadow: 0 3px 20px rgba(0,0,0,.4);
+  display: inline-block;
+  height: fit-content;
+  margin: 15px 10px;
+  padding: 0;
+  position: relative;
+  text-align: left;
+  text-align: initial;
 
 }
 
-.scaleDownImg{
-  object-fit: cover;
+.large-card .content-wrapper {
+  background-color: #1a203c;
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  position: relative;
 }
+
+
+.large-card .picture-wrapper {
+  padding: 20px 10px;
+  border-top-left-radius: 40px;
+  border-top-right-radius:40px;
+  position: relative;
+}
+
+
+*, :after, :before {
+  box-sizing: border-box;
+}
+
 </style>
 
 <script>
@@ -43,7 +89,7 @@ export default {
     return {list:undefined}
   },
   mounted(){
-    axios.get('https://wax.api.atomicassets.io/atomicassets/v1/assets').then((resp)=>{
+    axios.get('https://wax.api.atomicassets.io/atomicmarket/v1/sales').then((resp)=>{
           this.list=resp.data.data;
           console.log(resp)
         })
