063efb4efcb7a9fc4e1ccc660d73c4bbd7d35809
David Shen <zdshen99@gmail.com>
2020-11-03 11:30:03 -0700

    figuring out how to work with login function and import module with waxjs

--- bitcharityNFT/server/index.js
+++ bitcharityNFT/server/index.js
@@ -3,7 +3,15 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  playground: {
+    settings: {
+      'editor.theme': 'light',
+    }
+  }
+ });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
--- bitcharityNFT/server/schema.js
+++ bitcharityNFT/server/schema.js
@@ -14,10 +14,27 @@ const typeDefs = gql`
       market_fee: Float!
       data: String!
   }
+
+  type Data {
+    seller: String
+  }
+
+
+  type Actions {
+    account: String
+    name: String
+    authorization: [Authorization]
+    data: Data
+  }
+
+  type Authorization {
+    actor: String
+    permission: String
+  }
   type Mutation {
     createcol(author:String, collection_name:String,allow_notify:String, authorized_account:String, notify_accounts:String, market_fee:String,data:String):collections
     createschema(authorized_creator: String, collections_name:String, schema_name:String, schema_format: String):String
-
+    regmarket(creator:String,market_place_name: String):String
   }
   type schema {
     authorized_creator: String!,
