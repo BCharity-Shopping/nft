db20ae829ff488416e840de33937949103314aea
David Shen <zdshen99@gmail.com>
2020-11-03 09:53:56 -0700

    modifed file structure to basic level
    delete all redundant data and recreate the server project, right now does not have data

--- bitcharityNFT/server/index.js
+++ bitcharityNFT/server/index.js
@@ -1,57 +1,10 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema.js');
-const { createStore } = require('./utils');
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const LaunchAPI = require('./datasources/launch');
-const UserAPI = require('./datasources/user');
 
-const isEmail = require('isemail');
+const server = new ApolloServer({ typeDefs, resolvers });
 
-
-const store = createStore();
-const server = new ApolloServer({
-    context: async ({ req }) => {
-      // simple auth check on every request
-      const auth = req.headers && req.headers.authorization || '';
-      const email = Buffer.from(auth, 'base64').toString('ascii');
-      if (!isEmail.validate(email)) return { user: null };
-      // find a user by their email
-      const users = await store.users.findOrCreate({ where: { email } });
-      const user = users && users[0] || null;
-      return { user: { ...user.dataValues } };
-    },
-    // Additional constructor options
-  });
-
-
-const server = new ApolloServer({ 
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        launchAPI: new LaunchAPI(),
-        userAPI: new UserAPI({ store })
-      })
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
-
-
-const isEmail = require('isemail');
-
-const server = new ApolloServer({
-  context: async ({ req }) => {
-    // simple auth check on every request
-    const auth = req.headers && req.headers.authorization || '';
-    const email = Buffer.from(auth, 'base64').toString('ascii');
-    if (!isEmail.validate(email)) return { user: null };
-    // find a user by their email
-    const users = await store.users.findOrCreate({ where: { email } });
-    const user = users && users[0] || null;
-    return { user: { ...user.dataValues } };
-  },
-  // Additional constructor options
-});
-
-
-server.listen().then(({ url })=>{
-    console.log(`server ready at ${url}`);
-})
