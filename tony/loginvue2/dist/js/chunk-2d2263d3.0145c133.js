3fe66cbb94e2588a9b90a8a678c41e83f73d91ad
Ubuntu <ubuntu@ip-172-31-25-76.us-west-2.compute.internal>
2020-11-09 02:31:10 +0000

    add /dist

---
+++ tony/loginvue2/dist/js/chunk-2d2263d3.0145c133.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2263d3"],{e88b:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"market"},[n("h1",[e._v("Market")])])}],c={name:"Market"},s=c,l=n("2877"),u=Object(l["a"])(s,a,r,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d2263d3.0145c133.js.map
