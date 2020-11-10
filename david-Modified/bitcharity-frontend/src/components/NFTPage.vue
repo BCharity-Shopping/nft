52a336d931812e4b7a5d84073f6ebf539e129717
David Shen <zdshen99@gmail.com>
2020-11-09 22:01:33 -0700

    changed Query into dynamic query, start working on schema

--- david-Modified/bitcharity-frontend/src/components/NFTPage.vue
+++ david-Modified/bitcharity-frontend/src/components/NFTPage.vue
@@ -2,9 +2,9 @@
   <div>
       <P>My Collections</p>
       <v-if>
-        <p>this is {{WaxJs}}</p>
+        <p>this is {{getWax.userAccount}}</p>
       </v-if>
-      <ApolloQuery :query="require('../graphQL/collectionsinfo.gql')" :variables="{owner:'kdoaw.wam'}">
+      <ApolloQuery :query="require('../graphQL/collectionsinfo.gql')" :variables="{owner:getWax.userAccount}">
         <template v-slot="{ result: { data } }">
           <div v-for="(item, index) in data.atomicassets_assets" :key="item.collection_name">
             {{ index}}: {{item.collection_name}}
