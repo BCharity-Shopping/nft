33c9be7d216ae957ed4a72e90d08d13f02ed7f06
David Shen <zdshen99@gmail.com>
2020-11-11 17:44:22 -0700

    submit todays code right now

--- david-Modified/bitcharity-frontend/src/components/CreateSchema.vue
+++ david-Modified/bitcharity-frontend/src/components/CreateSchema.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <p>{{getWax}}</p>
         <p>Create Schema</p>
         <p>Schema_name</p>
         <input v-model="Schema_name" placeholder="Schema name">
@@ -9,43 +10,94 @@
         <p>Attribute Type</p><br />
         <p>text</p><br />
         <p>img</p>
-        <b-button @click="CreateSchema">Create Schema</b-button>
+        <table class="table">
+            <thead>
+                <tr>
+                    <tbody>
+                        <td><strong>Attribute_name</strong></td>
+                        <td><strong>Attribute_type</strong></td>
+                    
+                            <tr v-for="(row, index) in rows" :key=row.index>
+                                <td><input type="text" v-model="row.name"></td>
+                                <td><input type="text" v-model="row.type"></td>
+                                <td>
+                                    <a v-on:click="removeElement(index);" style="cursor: pointer">Remove</a>
+                                </td>
+                            </tr>
+                    </tbody>
+                </tr>
+            </thead>
+        </table>
+        <div>
+            <button class="button btn-primary" @click="addRow">Add row</button>
+            <button class="button btn-primary" @click="CreateSchema()">Add Schema</button>
+        </div>
     </div>
+
 </template>
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-    name:'CreateCollection',
     props:['wax','assetID'],
     data(){
         return{
-            Schema_name:""
-        }
-    },
-    computed: {
-      ...mapGetters([
-        'getWax'
-      ]),
-      collectionname:function(){
-          return this.collectionname
-      },
-      schemaFormat:function(){
-          var schemaformat=[
-              {
+            rows:[],
+            title:"",
+            countFirstboxInputID:0,
+            countSecondboxInputID:0,
+            Schema_name:"",
+            schemaformat:[{
                 "name":"name",
                 "type":"string"
               },
               {
                   "name":"img",
                   "type":"image"
-              }
-          ];
-          return this.schemaformat;
+              }]
+        }
+    },
+    watch:{
+
+    },
+    computed: {
+      ...mapGetters([
+        'getWax'
+      ]),
+      collectionName:function(){
+          return this.collectionname
+      },
+      schemaFormat:function(){
+         return this.schemaformat;
       }
     },
     methods:{
-       async CreateSchema(){
+        signalChange:function(evt){
+            console.log("this is changed")
+            this.$emit("change", evt)
+        },
+        addRow: function(){
+            var elem = document.createElement('tr');
+                    this.rows.push({
+                        name: "",
+                        type: "",
+                    });
+                    console.log(this.rows)
+                    return elem;
+        },
+        removeElement: function(index) {
+                    this.rows.splice(index, 1);
+        },
+         setFilename: function(event, row) {
+                    var file = event.target.files[0];
+                    row.file = file
+        },
+        async CreateSchema(){
+           for(var key in this.rows){
+               this.schemaformat.push({"name":this.rows[key].name,"type":this.rows[key].type})
+           }
+           console.log(this.schemaformat);
+
             if(!this.getWax.api){
                return console.log("Need to login first")
            }
@@ -55,19 +107,19 @@ export default {
                     account: 'atomicassets',
                     name: 'createschema',
                     authorization: [{
-                        actor: this.wax.userAccount,
+                        actor: this.getWax.userAccount,
                         permission:'active',
                     }],
                     data: {
                         authorized_creator:this.getWax.userAccount,
-                        collection_name:this.collectionname,
-                        Schema_name:this.schemaFormat,
+                        collection_name:this.collectionName,
+                        schema_format:this.schemaformat,
+                        schema_name:this.Schema_name
                     },
-                   }]
-               },
-               {
-                   blockBehind: 3,
-                   expireSecond: 30
+                }]
+               },{
+                   blocksBehind: 3,
+                   expireSeconds: 30
                })
                console.log(this.result);
                console.log("THIS AUTHOR IS "+this.author);
@@ -75,7 +127,7 @@ export default {
            catch(e){
                this.result=e
                console.log(e)
-           }       
+           }   
        }
     }
 }
