6bc284630f63818d09887977e4e686c26b8f6e00
Tony <wangzhen95@hotmail.com>
2020-11-12 13:46:17 -0700

    working on create offer

--- tony/loginvue2/src/components/TradeOffer.vue
+++ tony/loginvue2/src/components/TradeOffer.vue
@@ -6,7 +6,7 @@
     <div>
       <ApolloQuery
       :query="require('../graphql/getAccountInventory.gql')"
-      :variables="{owner}"
+      :variables="{owner:o}"
       >
         <template v-slot="{ result: { loading, error, data } }">
           <!-- Loading -->
@@ -17,52 +17,11 @@
 
           <!-- Result -->
           <div v-else-if="data" class="result apollo">
-
-            <div class="cards">
-              <div v-for="asset in data.atomicassets_assets" :key="asset.asset_id">
-                <div class="card">
-                  <div class="asset-img" v-if="asset.atomicassets_template!=null">
-                    <div class="asset-img-2" v-if="asset.atomicassets_template.immutable_data.img!=null">
-                      <img v-bind:src="ipfs + asset.atomicassets_template.immutable_data.img" width="100%" height="100%">
-                    </div>
-                  </div>
-                  <div class="asset-img" v-else-if="asset.immutable_data!=null">
-                    <div class="asset-img-2" v-if="asset.immutable_data.img!=null">
-                      <img v-bind:src="ipfs + asset.immutable_data.img" width="100%" height="100%">
-                    </div>
-                  </div>
-                  <div v-if="asset.atomicassets_asset_mints!=null">
-                    <p class="mint-num">#{{asset.atomicassets_asset_mints.template_mint}}</p>
-                  </div>
-                  <div class="col-name">
-                    {{asset.collection_name}}
-                  </div>
-                  <div class="asset-name" v-if="asset.atomicassets_template!=null">
-                    {{asset.atomicassets_template.immutable_data.name}}
-                  </div>
-                  <div class="asset-name" v-else>
-                    {{asset.immutable_data.name}}
-                  </div>
-                  <ApolloQuery
-                    :query="require('../graphql/getAssetBySale.gql')"
-                    :variables="{asset_id:asset.asset_id, state:state1}"
-                    >
-                      <template v-slot="{ result: { loading, error, data } }">
-                        <!-- Loading -->
-                        <div v-if="loading" class="loading apollo">Loading...</div>
-
-                        <!-- Error -->
-                        <div v-else-if="error" class="error apollo">An error occurred</div>
-
-                        <!-- Result -->
-                        <div v-else-if="data" class="result apollo">
-                          <div class="on-sale" v-if="data.atomicmarket_sales.length!=0">
-                            On Sale
-                          </div>
-                        </div>
-                      </template>
-                  </ApolloQuery>
-                </div>            
+            <div v-if="data.atomicassets_assets.length!=0">
+              <div class="cards">
+                <div v-for="asset in data.atomicassets_assets" :key="asset.asset_id">
+                  <AssetSmallCard class="unselected" :id="'asset'+asset.asset_id+role" :asset_id="asset.asset_id" @click.native="toggleOfferAsset(asset.asset_id)"/>
+                </div>
               </div>
             </div>
           </div>
@@ -75,16 +34,40 @@
 </template>
 
 <script>
+import AssetSmallCard from '@/components/AssetSmallCard.vue'
 export default {
   name: 'TradeOffer',
-  props: ['account_name'],
+  props: ['account_name','role'],
+  components: {
+    AssetSmallCard
+  },
   data () {
     return {
       owner: this.account_name,
-      ipfs: "http://ipfs.io/ipfs/",
-      state1: 1
+      selectedAssets: [],
+      key: 0
     }
   },
+  methods: {
+    toggleOfferAsset(asset_id) {
+      var id = "asset" + asset_id + this.role
+      if(this.selectedAssets.includes(asset_id)){
+        this.selectedAssets.splice(this.selectedAssets.indexOf(asset_id), 1)
+        document.getElementById(id).className = "unselected"
+      }
+      else {
+        this.selectedAssets.push(asset_id)
+        document.getElementById(id).className = "selected"
+      }
+      console.log("clicked " + asset_id + " on " + this.role)
+      console.log(this.selectedAssets)
+    }
+  },
+  computed: {
+    o: function() {
+      return this.owner
+    }
+  }
 }
 </script>
 
@@ -95,65 +78,16 @@ export default {
   flex-wrap: wrap;
   justify-content: space-around;
 }
-.card {
-  margin-bottom : 10px;
-  width: 200px;
-  height: 240px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: flex-start;
-  /*background-color: #666699;*/
+
+.unselected {
+  background-color: transparent;
   box-shadow: 0px 0px 10px 1px #262626;
-  margin-top: 10px;
-  color: grey;
   border-radius: 15px;
 }
 
-.col-name {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-}
-
-.asset-name {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-}
-
-.owner-name {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-}
-
-.asset-img-2 {
-  width: 100rem;
-  max-width: 150px;
-  height: 100%;
-}
-
-.asset-img {
-  margin-top: 5%;
-  margin-left: 5%;
-  width: 150px;
-  height: 150px;
-}
-
-.mint-num {
-  margin-top: 5px;
-  margin-left: 5px;
-}
-
-.on-sale {
-  text-align: center;
-  width: 100%;
-  margin-left: 10px;
-  color: white;
-  background-color: green;
+.selected {
+  background-color: transparent;
+  box-shadow: 0px 0px 10px 1px orange;
+  border-radius: 15px;
 }
 </style>
--- tony/loginvue2/src/views/New-Offer.vue
+++ tony/loginvue2/src/views/New-Offer.vue
@@ -7,8 +7,14 @@
     <div v-else>
       <h1>New Offer</h1>
       <b-button variant="warning">Send Trade Offer</b-button>
-      <div class="sender">
-        <TradeOffer :account_name="this.getWax.userAccount"/>
+      <input v-model="recipient" placeholder="Recepient">
+      <div class="offers">
+        <div class="sender">
+          <TradeOffer :key="this.getWax.userAccount" :account_name="this.getWax.userAccount" role="sender"/>
+        </div>
+        <div class="recipient">
+          <TradeOffer :key="this.recipient" :account_name="this.recipient" role="recipient"/>
+        </div>
       </div>
     </div>
   </div>
@@ -23,6 +29,11 @@ export default {
   components: {
     TradeOffer,
     Login,
+  },
+  data () {
+    return {
+      recipient: ""
+    }
   },
   computed: {
     ...mapGetters([
@@ -39,8 +50,21 @@ export default {
   max-width: 70%;
 }
 
+.offers {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
 .sender {
   max-width: 45%;
+  min-width: 45%;
+  background-color: rgb(230, 254, 255);
+}
+
+.recipient {
+  max-width: 45%;
+  min-width: 45%;
   background-color: rgb(230, 254, 255);
 }
 </style>
