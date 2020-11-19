f69518ac26ebaa6e06ccfa1072d76dddeb9b5f03
David Shen <zdshen99@gmail.com>
2020-11-18 22:43:38 -0700

    submit again
    Co-Authored-By: wztony <36280375+wztony@users.noreply.github.com>

---
+++ david-Modified/bitcharity-frontend/src/components/DonationProgress.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <b-progress :value="donationTotal" :max="listingPrice"></b-progress>
+    {{donationTotal}} WAX raised of {{listing_price/100000000}} WAX
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'DonationProgress',
+  props: ['account_name','sale_id','listing_price'],
+  data () {
+    return {
+      baseMemo: "donation to fundraiser #",
+      result: "",
+      baseURL: "https://wax.pink.gg/v2/history/get_actions?",
+      a: 5,
+      b: 10,
+    }
+  },
+  mounted () {
+    //console.log("API URL: " + this.url)
+    axios
+      .get(this.url)
+      .then(response => (this.result = response))
+  },
+  computed: {
+    url: function() {
+      return this.baseURL + "account=" + this.account_name + "&act.name=transfer&act.account=eosio.token"
+    },
+    donationTotal: function() {
+      if(this.result=="") {
+        return 0
+      }
+      else {
+        let actions = this.result.data.actions
+        let total = 0
+        for(var i = 0; i<actions.length; i++) {
+          if(this.memo == actions[i].act.data.memo) {
+            total = total + Number(actions[i].act.data.amount)
+          }
+        }
+        return total
+      }
+    },
+    memo: function() {
+      return this.baseMemo + this.sale_id
+    },
+    listingPrice: function() {
+      return this.listing_price/100000000
+    }
+  }
+}
+</script>
